dcf3eb69472ce1375b283fcef7dc9b61b424a571
glqdlt <dlfdnd0725@gmail.com>
2019-02-17 16:44:11 +0900

    Json 기반 API 추가

--- src/main/java/com/glqdlt/ex/springsecurity/ResourceRestController.java
+++ src/main/java/com/glqdlt/ex/springsecurity/ResourceRestController.java
@@ -1,13 +1,31 @@
 package com.glqdlt.ex.springsecurity;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * @see <a href="https://docs.spring.io/spring-security/site/docs/3.0.x/reference/el-access.html#el-common-built-in">https://docs.spring.io/spring-security/site/docs/3.0.x/reference/el-access.html#el-common-built-in</a>
+ */
 @RestController
 public class ResourceRestController {
 
+    /**
+     * jsr250Enabled : RolesAllowed('...')
+     * secureEnabled : Secured('...')  !! --> needs.. @EnableGlobalMethodSecurity(securedEnabled = true)
+     * prePostEnabled : PreAuthorize('...') , PostAuthorize('...') !! --> needs.. @EnableGlobalMethodSecurity(prePostEnabled = true)
+     * @return
+     */
+//    @PreAuthorize("hasRole('admin')")
+//    @Secured("hasRole('admin')")
     @PreAuthorize("hasRole('admin')")
     @GetMapping("/api/protected/admin")
     public String protectedAdminResource(){
@@ -20,9 +38,25 @@ public class ResourceRestController {
         return "user";
     }
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/")
     public String notProtectedResource(){
         return "hello";
+    }
+
+    /**
+     * @see <a href="https://www.baeldung.com/spring-security-method-security">https://www.baeldung.com/spring-security-method-security</a>
+     *
+     *
+     * consumes Content-Type 헤더를 참고함
+     * produces 는 accept 헤더를 참고함
+     * @return
+     */
+    @PreAuthorize("hasRole('admin')")
+    @PostMapping(value = "/api/protected/admin/json",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public HashMap protectedJsonData(@RequestBody  String param) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(param, HashMap.class);
     }
 
 }
--- src/test/java/com/glqdlt/ex/springsecurity/WebSecurityTests.java
+++ src/test/java/com/glqdlt/ex/springsecurity/WebSecurityTests.java
@@ -1,21 +1,34 @@
 package com.glqdlt.ex.springsecurity;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 /**
  * @author glqdlt
+ * WithMockUser vs WithUserDetail
+ * @see <a href="https://docs.spring.io/spring-security/site/docs/4.2.x/reference/html/test-method.html">https://docs.spring.io/spring-security/site/docs/4.2.x/reference/html/test-method.html</a>
+ *
  * @see <a href = "https://www.baeldung.com/spring-security-integration-tests">https://www.baeldung.com/spring-security-integration-tests</a>
  * 2019-02-17
  */
@@ -30,24 +43,35 @@ public class WebSecurityTests {
     @MockBean
     private UserDetailsService userDetailsService;
 
-    @WithMockUser(username = "testUser")
+
+    @WithAnonymousUser
     @Test
-    public void rootExpectedHelloString() throws Exception {
+    public void notProtectedResource_로그인을하지않은사용자() throws Exception {
         final String _stub = "hello";
         mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(_stub));
     }
 
-    @WithMockUser(username = "testUser")
+    @WithAnonymousUser
     @Test
-    public void rootExpectedHelloString_권한없어서접근이거부() throws Exception {
+    public void protectedAdminResource_로그인을하지않은사용자() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/protected/admin"))
-                .andExpect(MockMvcResultMatchers.status().is(403));
+                .andExpect(MockMvcResultMatchers.status().is(302))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("http://localhost/login"));
     }
+
+    @WithMockUser(username = "guest")
+    @Test
+    public void protectedAdminResource_로그인을했지만권한이없음() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/protected/admin"))
+                .andExpect(MockMvcResultMatchers.status().is(403))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
     @WithMockUser(username = "adminUser",roles = "admin")
     @Test
-    public void protectedAdminRole() throws Exception {
+    public void protectedAdminResource() throws Exception {
         final String _adminStub = "admin";
         mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/protected/admin"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -56,7 +80,7 @@ public class WebSecurityTests {
 
     @WithMockUser(username = "userUser",roles = "user")
     @Test
-    public void protectedUserRole() throws Exception {
+    public void protectedUserResource() throws Exception {
         final String _userStub = "user";
         mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/protected/user"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -65,11 +89,32 @@ public class WebSecurityTests {
 
     @WithMockUser(username = "adminUser",roles = "admin")
     @Test
-    public void protectedUserRole_AdminUser() throws Exception {
+    public void protectedUserResource_admin_권한이접근했을때() throws Exception {
         final String _userStub = "user";
         mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/protected/user"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(_userStub));
     }
 
+    /**
+     * WithUserDetails : <a href ='https://docs.spring.io/spring-security/site/docs/4.2.x/reference/html/test-method.html#test-method-withuserdetails'>@WithUserDetails</a>
+     * @see <a href="https://stackoverflow.com/questions/21749781/why-i-received-an-error-403-with-mockmvc-and-junit">https://stackoverflow.com/questions/21749781/why-i-received-an-error-403-with-mockmvc-and-junit</a>
+     * @throws Exception
+     */
+    @WithMockUser(username="jsonUser",roles = "admin")
+    @Test
+    public void protectedJsonData() throws Exception {
+        Map<String,String> body = new HashMap<>();
+        body.put("userName","jhun");
+        body.put("userId","glqdlt");
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(body);
+        mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.POST,"/api/protected/admin/json")
+                .content(content)
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(content))
+                .andDo(print());
+    }
 }
