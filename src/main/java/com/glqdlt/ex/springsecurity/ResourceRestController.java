24a86cc130f2de070b4c4434c012aaf6951ede7d
glqdlt <dlfdnd0725@gmail.com>
2019-02-17 10:05:29 +0900

    보호된 리소스 인증체계를 컨트롤러 어노테이션기반으로 처리

--- src/main/java/com/glqdlt/ex/springsecurity/ResourceRestController.java
+++ src/main/java/com/glqdlt/ex/springsecurity/ResourceRestController.java
@@ -1,12 +1,14 @@
 package com.glqdlt.ex.springsecurity;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceRestController {
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/api/protected/area")
     public String protectedResource(){
         return "good";
--- src/main/java/com/glqdlt/ex/springsecurity/SecurityConfig.java
+++ src/main/java/com/glqdlt/ex/springsecurity/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.glqdlt.ex.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -14,14 +18,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/protected/area").hasRole("ADMIN")
+//                .antMatchers("/api/protected/area").hasRole("ADMIN")
                 .anyRequest().permitAll()
                     .and()
-                .formLogin().successForwardUrl("/api/protected/area")
+                .formLogin()
                 .permitAll()
                     .and()
                 .logout()
                     .permitAll();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
